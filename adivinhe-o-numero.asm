271b7aff6f52414d4bf6b58eea219bd791b3268f
MariaRitaHott <139187119+MariaRitaHott@users.noreply.github.com>
2024-04-13 11:22:51 -0300

    Update adivinhe-o-numero.asm

--- adivinhe-o-numero.asm
+++ adivinhe-o-numero.asm
@@ -1,91 +1,130 @@
+
+#TRABALHO 1 - ORGANIZAÇÃO E ARQUITETURA DE COMPUTADORES.
+
+#Obejetivo: implementar um jogo em que o programa gera um numero aleatório e o usário deve adivinhar, por meio de tentativas, o número em questão.	
+			
+#LISTA DE REGISTRADORES E VARIAVEIS QUE ELES REPRESENTAM:
+# s0 = randNumber -> NRO ALEATORIO GERADO;
+# s1 = chute -> CHUTE FEILO PELO USUARIO;
+# -- lista encadeada --
+# s2 = SENTINELA DA LISTA ENCADEADA;
+# s3 = CONTADOR DO NÓS INSERIDOS;
+# -- funcao randNum --
+# a2 = mod -> MODULO IGUAL A 100;
+# a3 = a -> NUMERO PRIMO QUALQUER;
+# a4 = c -> NUMERO PRIMO QUALQUER;
+# a5 = seed -> NUMERO INTEIRO QUALQUER;
+# t0 = aux_comp -> AUXILIAR DA COMPARAÇÃO;
+# t1 = conta_aux -> AUXILIAR PARA REALIZAÇÃO DAS CONTAS;
+# a1 = resultado -> RESULTADO DE NUMERO ALEATORIO FINAL;
+# -- função para lista encadeada --
+# a1 -> ENDEREÇO DO SENTINELA ;
+# a2 -> VALOR DO NUMERO QUE SERÁ INSERIDO;
+# t0 -> VARIÁVEL AUXILIAR PARA ARMAZENAR O ANTIGO COMEÇO DA LISTA;
+# t1 -> VARIÁVEL PARA ARMAZENAR O ENDEREÇO DA PARTE DO PONTEIRO PARA O NÓ;
+	
 	.data
-	.align 0	#alinhando para string
+	.align 0
+	
+	#string usadas na interface com o usário;
 initMsg:.asciz "Bem-vindo! Este é o jogo 'Adivinhe o número'!\nO computador escolheu um número entre 1 e 100, você consegue adivinhar qual é? Faça seu chute!\n"
-			#mensagens de boas-vindas e instruções
-chuteMsg: .asciz "Chute: " #diz qual foi o chute do usuário
-randMsg: .asciz "\nNúmero aleatório: " #diz qual foi o nro aleatorio gerado
-mtAlto:	.asciz "Seu chute foi mais alto que o nro correto, tente novamente!\n"
-mtBaixo: .asciz "Seu chute foi mais baixo que o nro correto, tente novamente!\n"
-finalMsg: .asciz "\nFim! Obrigada por jogar! :D\n" #finalização
+chuteMsg: .asciz "Chute: " 
+randMsg: .asciz "\nNúmero aleatório: " 
+mtAlto:	.asciz "Seu chute foi mais alto que o numero correto, tente novamente!\n"
+mtBaixo: .asciz "Seu chute foi mais baixo que o numero correto, tente novamente!\n"
+finalMsg: .asciz "\nFim! Obrigada por jogar! :D\n" 
 pulaLinha: .asciz "\n"
 espaco:	   .asciz " "	
-	.align 2	#alinhando para inteiro
 
-
+	.align 2	
 	.text
 	.align 2
 	.globl main
 	
 main:
-	addi a7, zero, 4 #printando msg de boas vindas e instruções
+	#printando msg de boas vindas e instruções;
+	addi a7, zero, 4 
 	la a0, initMsg
 	ecall			
 	
 	jal randNum
-	add s0, zero, a1 #s0 = nro aleatorio entre 0 e 100
+	add s0, zero, a1 #s0 recebe o numero aleatorio entre 0 e 100;
 	
-	li a7, 4	#printando o nro aleatorio que foi gerado --
-	la a0, randMsg
-	ecall
+	#li a7, 4	#printando o nro aleatorio que foi gerado --
+	#la a0, randMsg
+	#ecall
 	
-	addi a7, zero, 1
-	add a0, zero, s0
-	ecall		#--
+	#addi a7, zero, 1
+	#add a0, zero, s0
+	#ecall		#--
 	
-	li a7, 4	#pula linha
-	la a0, pulaLinha
-	ecall
+	#li a7, 4	#pula linha
+	#la a0, pulaLinha
+	#ecall
 	
-	addi a7, zero, 5 #solicitando entrada de inteiro (chute de numero)
+	#solicitando entrada de inteiro (chute);
+	addi a7, zero, 5 
 	ecall		
 	
-	add s1, zero, a0 #armazenando o chute em s1
+	add s1, zero, a0 #armazenando o chute em s1;
 	
-	li s3, 0 # inicia o contador como 0
+	# Para mostrar o numero total de tentativas será usado um contador e ele será inicalizado com zero;
+	li s3, 0 
 	jal criar_lista
 	
-	mv s2, a1 # guarda o sentinela em s2
+	mv s2, a1 # guarda o sentinela em s2;
+	mv a1, s2 #passa o sentinela como parâmetro; 
+	mv a2, s1 # passa o numero que tem que ser inserido como parâmentro;
 	
-	mv a1, s2 #passa o sentinela como parâmetro 
-	mv a2, s1 # passa o numero que tem que ser inserido como parâmentro
 	jal inserir_no
 loop_chute:
-	beq s1, s0, sai_chute
+
+	beq s1, s0, sai_chute #condição de saida do loop_chute;
 	
-	bge s1, s0, chute_alto
+	bge s1, s0, chute_alto  #pula para a condução chute_alto se s1>=s0;
 	
-	addi a7, zero, 4 #printando msg de número muito baixo
+	#printando msg de número muito baixo;
+	addi a7, zero, 4 
 	la a0, mtBaixo
 	ecall
 	
-	addi a7, zero, 5 #solicitando entrada de inteiro (chute de numero)
+	#solicitando entrada do usuário;
+	addi a7, zero, 5 
 	ecall		
 	
-	add s1, zero, a0 #armazenando o chute em s1
+	add s1, zero, a0 #armazenando o chute em s1;
 	
-	mv a1, s2 #passa o sentinela como parâmetro 
-	mv a2, s1 # passa o numero que tem que ser inserido como parâmentro
+	mv a1, s2 #passa o sentinela como parâmetro ;
+	mv a2, s1 # passa o numero que tem que ser inserido como parâmentro;
+	
 	jal inserir_no
 	
 	j loop_chute
 	
 chute_alto:
-	addi a7, zero, 4 #printando msg de número muito alto
+
+	#imprimi a mensagem de número muito alto;
+	addi a7, zero, 4 
 	la a0, mtAlto
 	ecall
 	
-	addi a7, zero, 5 #solicitando entrada de inteiro (chute de numero)
+	#solicitando entrada do usuário;
+	addi a7, zero, 5 
 	ecall		
 	
-	add s1, zero, a0 #armazenando o chute em s1
+	add s1, zero, a0 #armazenando o chute em s1;
 	
-	mv a1, s2 #passa o sentinela como parâmetro 
-	mv a2, s1 # passa o numero que tem que ser inserido como parâmentro
+	mv a1, s2 #passa o sentinela como parâmetro; 
+	mv a2, s1 # passa o numero que tem que ser inserido como parâmentro;
+	
 	jal inserir_no
+	
 	j loop_chute
 	
 sai_chute:
-	addi a7, zero, 4 #printando o chute--
+
+	#imprimi a tentativa correta do usário no final;
+	addi a7, zero, 4 
 	la a0, chuteMsg
 	ecall
 	
@@ -93,7 +132,8 @@ sai_chute:
 	add a0, zero, s1
 	ecall		#--
 	
-	li a7, 4	#printando o nro aleatorio que foi gerado --
+	#imprimi o numero aleatorio que foi gerado;
+	li a7, 4
 	la a0, randMsg
 	ecall
 	
@@ -101,14 +141,17 @@ sai_chute:
 	add a0, zero, s0
 	ecall		#--
 	
-	li a7, 4	#pula linha
+	#pula uma linha para questões de organização;
+	li a7, 4	
 	la a0, pulaLinha
 	ecall
 	
+	#imprime a lista;
 	lw a1, 0(s2)
-	jal imprimir_lista #imprime a lista
+	jal imprimir_lista 
 	
-	li a7, 4	#pula linha
+	#pula uma linha para questões de organização;
+	li a7, 4
 	la a0, pulaLinha
 	ecall
 	
@@ -116,113 +159,102 @@ sai_chute:
 	add a0, zero, s3
 	ecall	
 	
-	
-	
-	
-	
-	addi a7, zero, 4 #printando msg final
+	#imprimindo mensagem final;
+	addi a7, zero, 4 
 	la a0, finalMsg
 	ecall
 	
-	
-	addi a7, zero, 10 #finalizando programa
+	#finalizando programa;
+	addi a7, zero, 10 
 	ecall
 
-randNum:		#função que gera o numero pseudoaleatorio
-			#mod -> a2 = 100; a -> a3 = 223; c -> a4 = 607; seed -> a5 = ecall;
-			#aux_comp -> t0; conta_aux -> t1; resultado -> a1
-	addi a2, zero, 100 #queremos um numero ate 100, entao mod = 100
-	addi a3, zero, 23 #numero primo mto grande qualquer
-	addi a4, zero, 17 #outro numero primo mto grande qualquer
+randNum:		
+	#função que gera o numero pseudoaleatorio;
+	#Representação das variáveis do pseudocódigo em linguagens de alto nivel, seu registrador correspondente e o valor que possui:
+	# - mod -> a2 = 100; 
+	# - a -> a3 = 223; 
+	# - c -> a4 = 607; 
+	# - seed -> a5 = ecall;
+	# - aux_comp -> t0;  (auxiliar de comparação)
+	# - conta_aux -> t1;  (contador auxiliar)
+	# - resultado -> a1;
 	
-	addi a7, zero, 41 #gerando a seed com a syscall de RandInt--
+	addi a2, zero, 100 #queremos um numero ate 100, entao mod = 100;
+	addi a3, zero, 23 #numero primo muito grande e qualquer;
+	addi a4, zero, 17 #outro numero primo muito grande e qualquer;
+	
+	#gerando a seed com a syscall de RandInt (presente do rars);
+	addi a7, zero, 41 
 	ecall
-	add a5, zero, a0 #--
+	add a5, zero, a0 
 	
-	addi t0, zero, 1 #aux_comp (auxiliar de comparação) = 1
+	addi t0, zero, 1 #aux_comp = 1;
 	
-	blt a5, t0, randNum #se o nro gerado for menor que 1, repete a funçao do começo
+	blt a5, t0, randNum #se o número gerado for menor que 1, repete a funçao desde o começo;
 	
+	#manipulação dos registradores:
 	rem a5, a5, a2	# seed = seed % 100
 	mul t1, a5, a3	# conta_aux = seed * a
 	add t1, t1, a4	# conta_aux = conta_aux + c
 	rem a1, t1, a2	# resultado = conta_aux % 100
 	
-
-
 	jr ra
+	
 inserir_no:
-	# a1 -> endereço do sentinela
-	# a2 -> valor do numero que eu tenho que inserir
-	# t0 -> variável auxiliar para armazenar o antigo começo da lista
-	# t1 -> variável pra armazenar o endereço da parte de ponteiro do nó
-	# nessa lista encadeada a gente insere no fim (FIFO) 
+
+	# nesta implementação a incersão é feita no final (FIFO) ;
+	# aloca o nó, os primeiro 4 bytes para 0 numero e os 4 últimos pra apontar para o proximo nó;
 	li a7, 9
-	li a0, 8 # aloca o no, os primeiro 4 bytes pro numero e os úlmtios 4 pra apontar pro prox nó 
+	li a0, 8  
 	ecall
 	
-	sw a2, 0(a0) # copia o numero do usuário pros primeiro 4 bytes do nó
+	sw a2, 0(a0) # copia o numero do usuário para os primeiro 4 bytes do nó;
         
-        lw t0, 0(a1) # armazena o antigo começo da lista em t0
-        addi t1, a0, 4 # armazazena a parte de ponteiro do nó no t1
-        sw t0, 0(t1) # faz o nó apontar para o antigo começo da lista
-                 # armazena o antigo começo da lista nos útimos 4 bytes do nó 
-                 
-                 
-        sw a0, 0(a1) # faz o sentinela apontar pro novo no
-        addi s3, s3, 1 # incrementa o contador 
+        lw t0, 0(a1) # armazena o antigo começo da lista em t0;
+        
+        addi t1, a0, 4 # armazena a parte de ponteiro do nó no t1;
+        
+        sw t0, 0(t1) # faz o nó apontar para o antigo começo da lista;
+        
+        sw a0, 0(a1) # faz o sentinela apontar pro novo no;
+        
+        addi s3, s3, 1 # incrementa o contador ;
+        
         j return
         
 criar_lista: 
-	# a1 -> armazena 
+	 
+	#alocação de memória para o sentilena;
 	li a7, 9
-	li a0, 4 #aloca o sentinela, que é um ponteiro 
+	li a0, 4 
 	ecall
 	
-	
-	mv a1,a0  #bota o endereço do sentinela no registrador a1
+	mv a1,a0  #move o endereço do sentinela para o registrador a1;
 	
 	li t0, 0
-	sw t0, 0(a1) # faz o sentinla apontar pra NULL
+	
+	sw t0, 0(a1) #faz o sentinla apontar para NULL;
         
         j return
+        
 imprimir_lista: 
-        # a1 -> recebe o nó atual	
-	beq a1, x0,  return # volta pra main 
+
+	beq a1, x0,  return  #retorna para a main;
 	
-	 # printa o numero 
+	#imprimi a tentativa do usário;
 	lw a0, 0(a1)
 	li a7, 1
 	ecall
 	
-	# da um espaço
+	#imprimi um espaço entre as tentativas;
 	la a0,espaco
 	li a7, 4
 	ecall
 	
-	lw a1, 4(a1) # bota o próximo nó para ser processado
-	
+	lw a1, 4(a1) #pula para o proximo nó;	
 	j imprimir_lista
-	
-	
-
-		
-         
+	        
 return:
-	jr ra # volta pra o lugar que a função foi chamada
-	
-#LISTA DE REGISTRADORES E VARIAVEIS QUE ELES REPRESENTAM
-
-# s0 = randNumber -> NRO ALEATORIO GERADO
-# s1 = chute -> CHUTE FEILO PELO USUARIO
-# s2 = sentinela da lista encadeada
-# s3 = contador de nós inseridos 
-# -- funcao randNum --
-# a2 = mod -> MODULO IGUAL A 100
-# a3 = a -> NRO PRIMO QUALQUER
-# a4 = c -> NRO PRIMO QUALQUER
-# a5 = seed -> NRO INTEIRO QUALQUER
-# t0 = aux_comp -> AUXILIAR DA COMPARAÇÃO
-# t1 = conta_aux -> AUXILIAR PARA REALIZAÇÃO DAS CONTAS
-# a1 = resultado -> RESULTADO DE NRO ALEATORIO FINAL
+	#apenas uma função de modularização;
+	jr ra
 
